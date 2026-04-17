7c8559fc5e50829a78f6ad930979fee64e477cf5
Subbiah Kumar <subbiah@Subbiahs-MacBook-Air.local>
2026-04-17 22:27:32 +0800

    Arithmetic operations using variables

---
+++ variable_using_operations.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+## this script for arithemetic operations
+a=10
+b=20
+echo "a: $a"
+echo "b: $b"
+echo "a+b: $((a+b))"
+echo "a-b: $((a-b))"
+echo "a*b: $((a*b))"
+echo "a/b: $((a/b))"
+echo "a%b: $((a%b))"    

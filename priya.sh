d9e5aaad056a4547348f7464b37d174f3b4e1833
Subbiah Kumar <subbiah@Subbiahs-MacBook-Air.local>
2026-04-25 04:36:19 +0800

    variable using operators

---
+++ priya.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+## this script for arithemetic operations
+a=10
+b=233
+echo "a: $a"
+echo "b: $b"
+echo "a+b: $((a+b))"
+echo "a-b: $((a-b))"
+echo "a*b: $((a*b))"
+echo "a/b: $((a/b))"
+echo "a%b: $((a%b))"    
